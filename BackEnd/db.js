d3729a191df244d708e67bf16037cb7e95e8f02e
nikhilamin073 <nikhilamin073@github.com>
2018-10-20 11:26:33 +0530

    setting DB connection

---
+++ BackEnd/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/notesbynerdsdb', (err) => {
+    if (!err)
+        console.log('MongoDB connection for Notes By Nerds succeeded.');
+    else
+        console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+});
+
+module.exports = mongoose;
