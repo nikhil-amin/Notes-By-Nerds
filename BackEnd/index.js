2fc38894757e275ec303c9c3fad402c51d58c85f
nikhilamin073 <nikhilamin073@github.com>
2018-10-20 12:04:43 +0530

    calling feedback controller from index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { mongoose } = require('./db.js');
+var feedbackController = require('./controllers/feedbackController.js');
 
 var app = express();
 app.use(bodyParser.json());
 
 app.listen(3000, () => console.log('Server started at port : 3000'));
+
+app.use('/feedbacks', feedbackController);
