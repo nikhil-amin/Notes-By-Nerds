4073cb4da1f22d0566030974772dd3ecb7cb910f
nikhilamin073 <nikhilamin073@github.com>
2018-10-20 16:18:55 +0530

    creating nav-bar component

---
+++ FrontEnd/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+  username:String='Username';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
