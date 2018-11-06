738ab3a927aa394f93e495302c69c8d80378b772
nikhil-amin <aminnikhil073@gmail.com>
2018-11-06 23:30:00 +0530

    Online Mongoose Database

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/notesbynerdsdb', { useNewUrlParser: true }, (err) => {
+//ONLINE DATABASE
+mongoose.connect('mongodb://root:root073@ds147233.mlab.com:47233/notesbynerdsdb', { useNewUrlParser: true }, (err) => {
     if (!err)
         console.log('MongoDB connection for Notes By Nerds succeeded.');
     else
         console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
 });
+
+//OFFLINE DATABASE
+// mongoose.connect('mongodb://127.0.0.1:27017/notesbynerdsdb', { useNewUrlParser: true }, (err) => {
+//     if (!err)
+//         console.log('MongoDB connection for Notes By Nerds succeeded.');
+//     else
+//         console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+// });
 
 mongoose.set('useCreateIndex', true)
 
