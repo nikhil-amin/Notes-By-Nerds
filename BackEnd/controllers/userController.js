63340de96a9cf7fbd606b3a7107e8e9b7cee8828
nikhil-amin <aminnikhil073@gmail.com>
2018-11-06 20:30:55 +0530

    Backend for Registration model

---
+++ BackEnd/controllers/userController.js
@@ -0,0 +1,24 @@
+const express = require('express');
+var router = express.Router();
+
+var { User } = require('../models/user');
+
+router.post('/register', (req, res, next) => {
+    var user = new User ({
+        fullName: req.body.fullName,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    user.save((err, docs) => {
+        if(!err) { res.send(docs); }
+        else { 
+            if (err.code == 11000)
+                res.status(422).send(['Duplicate email adrress found.']);
+            else
+                // return next(err);
+                console.log('Error in registering user: ' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+module.exports = router;
--- BackEnd/index.js
+++ BackEnd/index.js
@@ -4,11 +4,14 @@ const cors = require('cors');
 
 const { mongoose } = require('./db.js');
 var feedbackController = require('./controllers/feedbackController.js');
+var userController = require('./controllers/userController.js');
 
 var app = express();
+
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://127.0.0.1:4200' }));
 
 app.listen(3000, () => console.log('Server started at port : 3000'));
 
 app.use('/feedbacks', feedbackController);
+app.use('/api', userController);
