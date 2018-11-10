857eb3321f3b9b00d66210d7a37551938baa185b
nikhil-amin <aminnikhil073@gmail.com>
2018-11-10 11:49:55 +0530

    GET method to retrive notes collection

--- BackEnd/controllers/noteController.js
+++ BackEnd/controllers/noteController.js
@@ -1,7 +1,15 @@
 const express = require('express');
 var router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
 
 var { Note } = require('../models/note');
+
+router.get('/', (req, res) => {
+    Note.find((err, docs) => {
+        if (!err) { res.send(docs); }
+        else { console.log('Error in Retriving Notes: ' + JSON.stringify(err, undefined, 2)); }
+    });
+});
 
 router.post('/', (req, res) => {
     var note = new Note ({
