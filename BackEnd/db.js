7247a6a4dcffcf10bf83bc036073d4a9af78c462
nikhil-amin <aminnikhil073@gmail.com>
2018-12-06 20:33:25 +0530

    minor changes

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //ONLINE DATABASE
 mongoose.connect('mongodb://root:root073@ds147233.mlab.com:47233/notesbynerdsdb', { useNewUrlParser: true }, (err) => {
     if (!err)
-        console.log('MongoDB connection for Notes By Nerds succeeded.');
+        console.log('[MLab] MongoDB connection for Notes By Nerds succeeded.');
     else
         console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
 });
@@ -11,7 +11,7 @@ mongoose.connect('mongodb://root:root073@ds147233.mlab.com:47233/notesbynerdsdb'
 //OFFLINE DATABASE
 // mongoose.connect('mongodb://127.0.0.1:27017/notesbynerdsdb', { useNewUrlParser: true }, (err) => {
 //     if (!err)
-//         console.log('MongoDB connection for Notes By Nerds succeeded.');
+//         console.log('[Local] MongoDB connection for Notes By Nerds succeeded.');
 //     else
 //         console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
 // });
